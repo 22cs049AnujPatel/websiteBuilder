c44fde9348ac828e6fbeb0a3372a220ee24c7178
22cs049AnujPatel <120238185+22cs049AnujPatel@users.noreply.github.com>
2026-04-02 17:26:05 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ const port=process.env.PORT || 5000
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"https://websitebuilder-1-f2k1.onrender.com",
+    origin:"https://websitebuilder-1-q829.onrender.com",
     credentials:true
 }))
 app.use("/api/auth",authRouter)
